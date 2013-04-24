307ec4e2b151d123a4098df8370f16101d61aa73
keijiro <keijiro@gmail.com>
2013-04-24 22:35:10 +0900

    Aded spawner.

--- Assets/PlanetMove.js
+++ Assets/PlanetMove.js
@@ -5,7 +5,12 @@ var ecc = 0.3;
 var omega = 1.0;
 var phi = 0.0;
 
-private var theta = 0.0;
+private var theta = Mathf.PI;
+
+function Start() {
+	radius = transform.position.magnitude / 2;
+	phi = Mathf.Atan2(-transform.position.y, -transform.position.x);
+}
 
 function Update() {
 	var r = radius / (1.0 + ecc * Mathf.Cos(theta));
---
+++ Assets/PlanetSpawner.js
@@ -0,0 +1,14 @@
+#pragma strict
+
+var prefab : GameObject;
+
+private function SpawnPlanet(pos : Vector3) {
+	Instantiate(prefab, pos, Quaternion.identity);
+}
+
+function Update() {
+	if (Input.GetMouseButton(0)) {
+		var pos = Vector3(Input.mousePosition.x, Input.mousePosition.y, Mathf.Abs(Camera.main.transform.position.z));
+		SpawnPlanet(Camera.main.ScreenToWorldPoint(pos));
+	}
+}
