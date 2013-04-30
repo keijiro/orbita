2b93ad7ae7e20a94657ea25ad42cacaed06c5d72
Keijiro Takahashi <keijiro@gmail.com>
2013-05-01 07:56:01 +0900

    Automatic color generation.

--- Assets/Scripts/PlanetPalette.js
+++ Assets/Scripts/PlanetPalette.js
@@ -1,11 +1,15 @@
 #pragma strict
 
-var colors : Color[];
+var variation = 32;
 
 private static var index = 0;
 
 function PickColor() {
-	var color = colors[index++];
-	if (index >= colors.Length) index = 0;
-	return color;
+	var x = 6.0 * index / variation;
+	if (++index >= variation) index = 0;
+	return Color(
+		Mathf.Clamp01(Mathf.Abs(3.0 - x) - 1.0),
+		Mathf.Clamp01(2.0 - Mathf.Abs(2.0 - x)),
+		Mathf.Clamp01(2.0 - Mathf.Abs(4.0 - x))
+	);
 }
