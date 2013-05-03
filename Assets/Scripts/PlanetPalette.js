86165e7025933acf9f81238970e2227d950e3b63
Keijiro Takahashi <keijiro@gmail.com>
2013-05-03 10:27:18 +0900

    Redesign (coloring).

--- Assets/Scripts/PlanetPalette.js
+++ Assets/Scripts/PlanetPalette.js
@@ -1,6 +1,7 @@
 #pragma strict
 
-var variation = 32;
+var variation = 16;
+var brightness = 1.0;
 
 private static var index = 0;
 
@@ -8,8 +9,8 @@ function PickColor() {
 	var x = 6.0 * index / variation;
 	if (++index >= variation) index = 0;
 	return Color(
-		Mathf.Clamp01(Mathf.Abs(3.0 - x) - 1.0),
-		Mathf.Clamp01(2.0 - Mathf.Abs(2.0 - x)),
-		Mathf.Clamp01(2.0 - Mathf.Abs(4.0 - x))
+		Mathf.Clamp01(Mathf.Abs(3.0 - x) - 1.0) * brightness,
+		Mathf.Clamp01(2.0 - Mathf.Abs(2.0 - x)) * brightness,
+		Mathf.Clamp01(2.0 - Mathf.Abs(4.0 - x)) * brightness
 	);
 }
