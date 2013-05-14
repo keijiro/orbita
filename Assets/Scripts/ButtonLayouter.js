85cd9f27cdc150b7ac2cea5abea7212d1bf02fe5
Keijiro Takahashi <keijiro@gmail.com>
2013-05-15 08:41:00 +0900

    Redesign

--- Assets/Scripts/ButtonLayouter.js
+++ Assets/Scripts/ButtonLayouter.js
@@ -1,6 +1,12 @@
 #pragma strict
 
+var mode = 0;
+
 function Start() {
 	var z = Mathf.Abs(camera.main.transform.position.z);
-	transform.position += camera.main.ScreenToWorldPoint(Vector3(0, Screen.height, z));
+	if (mode == 0) {
+		transform.position += camera.main.ScreenToWorldPoint(Vector3(0, Screen.height, z));
+	} else {
+		transform.position += camera.main.ScreenToWorldPoint(Vector3(Screen.width, Screen.height, z));
+	}
 }
--- Assets/Scripts/TouchInput.js
+++ Assets/Scripts/TouchInput.js
@@ -20,7 +20,7 @@ private function DoTouch(screenCoord : Vector3) {
 	var ray = Camera.main.ScreenPointToRay(screenCoord);
 	var hit : RaycastHit;
 	if (Physics.Raycast(ray, hit) && hit.collider) {
-		if (hit.collider.name == "Decrement Button") {
+		if (hit.collider.name == "Erase Button") {
 			spawner.RemoveOldestPlanet();
 			return;
 		}
