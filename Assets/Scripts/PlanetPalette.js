5922f83360609eaefbc4eebb432bf9b47ec46b73
Keijiro Takahashi <keijiro@gmail.com>
2013-05-15 21:35:48 +0900

    Coloring.

--- Assets/Scripts/PlanetPalette.js
+++ Assets/Scripts/PlanetPalette.js
@@ -1,15 +1,17 @@
 #pragma strict
 
 var variation = 32;
+var luma = 0.2;
 
 private static var index = 0;
 
 function PickColor() {
 	var x = 6.0 * index / variation;
 	if (++index >= variation) index = 0;
-	return Color(
+	var color = Color(
 		Mathf.Clamp01(Mathf.Abs(3.0 - x) - 1.0),
 		Mathf.Clamp01(2.0 - Mathf.Abs(2.0 - x)),
 		Mathf.Clamp01(2.0 - Mathf.Abs(4.0 - x))
 	);
+	return Color.Lerp(color, Color.white, luma);
 }
